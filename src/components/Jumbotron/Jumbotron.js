968dc4bb519b2bcb44d5b4fe1927ed55a5bcbc80
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-03-15 11:43:30 +0000

    Initial Commit

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,22 @@
+import {React,Image} from 'react';
+import './Jumbotron.css';
+
+const Jumbotron = () => (
+  <div className="Jumbotron">
+    <h1>Ambrosia</h1>
+    <h3>feast like a god</h3>
+    <span id='image-group'>
+      <img src={require('../../images/pancake.jpg')}/>
+      <img src={require('../../images/pancake.jpg')}/>
+      <img src={require('../../images/pancake.jpg')}/>
+      <img src={require('../../images/pancake.jpg')}/>
+      <img src={require('../../images/pancake.jpg')}/>
+    </span>
+  </div>
+);
+
+Jumbotron.propTypes = {};
+
+Jumbotron.defaultProps = {};
+
+export default Jumbotron;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Navbar.css';
+
+const Navbar = () => (
+  <div className="Navbar">
+          <button>home</button>
+          <button>about</button>
+          <button>gallery</button>
+          <button>contact</button>
+  </div>
+);
+
+
+export default Navbar;
