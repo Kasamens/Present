cefad279e465020d8559af2a53c047d33deb7713
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-03-15 23:20:48 +0000

    Added content to About page cards

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -10,10 +10,41 @@ function About(){
             <div id='card-group'>
                 <div class='card' id='menu-card'>
                     <h2>MENU</h2>
+                    <br></br>
+                    <div class="menu-item">
+                        <span>Pizza</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="menu-item">
+                        <span>Pizza</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="menu-item">
+                        <span>Pizza</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="menu-item">
+                        <span>Pizza</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="menu-item">
+                        <span>Pizza</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="menu-item">
+                        <span>Pizza</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="menu-item">
+                        <span>Pizza</span>
+                        <span>$50</span>
+                    </div>
                     
                 </div>
                 <div class='card' id='welcome-card'>
                     <h2>WELCOME</h2>
+                    <br></br>
+                    <img src= {require('../../images/pancake.jpg')}/>
                 </div>
                 <div class='card' id='services-card'>
                     <h2>SERVICES</h2>
