d1aed5208c3f6e184bd67528cd34c98ccd30adae
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-03-15 21:26:25 +0000

    Added routing and additional pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
 import './components/Navbar/Navbar'
 import Home from './pages/Home'
+import About from './pages/About';
 import Navbar from './components/Navbar/Navbar';
-import Jumbotron from './components/Jumbotron/Jumbotron'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Navbar/>
-      </header>
-      <main>
-        <Home/>
-      </main>
-      <footer>
-        footer
-      </footer>
+    <Router>
+      <div className="App">
+        <header>
+          <Navbar/>
+        </header>
+        <main>
+          <Routes>
+              <Route exact path='/' element={<Home/>}/>
+              <Route exact path='/about' element={<About/>}/>
+          </Routes>
+        </main>
+        <footer>
+          footer
+        </footer>
     </div>
+  </Router>
   );
 }
 
