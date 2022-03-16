9c8947324b9a4b4254805edcd6b8b298cacd0cee
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-03-16 11:36:03 +0000

    Added form to contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,7 +1,22 @@
+import './Contact.css'
+
 function Contact(){
     return(
         <>
-        Contact Page
+            <div id='contact-layout'>
+                <h1>Contact Us:</h1>
+                <h3>For more information concerning how best we can 
+                    serve you, please reach out to us.
+                </h3>
+                <div class="doube-container">
+                    <form>
+                        <input type="text" placeholder="Full Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="text" placeholder="Phone Number" />
+                    </form>
+                    
+                </div>
+            </div>
         </>
     )
 }
