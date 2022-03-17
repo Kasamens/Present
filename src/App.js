90f56d2ea566f1c4ace1654b7117c22c220de808
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-03-17 11:26:21 +0000

    Removed borders and changed font to white

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Contact from './pages/Contact/Contact';
 import Gallery from './pages/Gallery/Gallery';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Background from './images/pancake.jpg'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{background: `url(${Background})`}}>
         <header>
           <Navbar/>
         </header>
