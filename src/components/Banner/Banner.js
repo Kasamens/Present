1953c7b21d434865519137f3244ec315605c271e
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-03-17 20:11:19 +0000

    Testing CI/CD

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css'
 const Banner = () =>{
     return(
         <div id="banner">
-            <h1>Ambrosia</h1>
+            <h1>AmbrosiaZee</h1>
             <h3>Feast like a god</h3>
          </div>
     )
